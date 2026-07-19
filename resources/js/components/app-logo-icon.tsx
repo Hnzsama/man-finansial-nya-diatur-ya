1574a62de50ad3dec4cbcc78ddc95def9b3590bf
Hnzsama <hnzsama@gmail.com>
2026-07-20 01:27:08 +0700

    style: change AppLogoIcon to render new app icon image (/apple-touch-icon.png)

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,13 +1,12 @@
-import type { SVGAttributes } from 'react';
+import type { ImgHTMLAttributes } from 'react';
 
-export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
+export default function AppLogoIcon({ className, ...props }: ImgHTMLAttributes<HTMLImageElement>) {
     return (
-        <svg {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M21 7.28V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-2.28c.59-.35 1-.98 1-1.72V9c0-.74-.41-1.37-1-1.72zM20 9v6h-7V9h7zM5 19V5h14v2h-6c-1.1 0-2 .9-2 2v6c0 1.1.9 2 2 2h6v2H5z"
-            />
-        </svg>
+        <img
+            src="/apple-touch-icon.png"
+            alt="App Logo"
+            className={className}
+            {...props}
+        />
     );
 }
